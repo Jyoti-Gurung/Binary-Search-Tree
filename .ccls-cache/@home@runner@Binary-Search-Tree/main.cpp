5e67f717ea4d06895cac044845740c826c132221
Jyoti Gurung <74558610+Jyoti-Gurung@users.noreply.github.com>
2022-05-03 05:36:36 +0000

    New Improvements

--- .ccls-cache/@home@runner@Binary-Search-Tree/main.cpp
+++ .ccls-cache/@home@runner@Binary-Search-Tree/main.cpp
@@ -8,54 +8,62 @@ Project: Binary Search Tree; Add, Remove and look up numbers
 
 using namespace std;
 
-// Set up functions up here; fleshed out under main 
-void display(int * input, int size, int depth, int control);
-void peak(int * input, int size);
-void deleted(int * input, int size);
+struct node { 
+	int data; 
+	node * left; 
+	node * right; 
+};
+
+node * add(node* & root, int & data);
+void peak(node *root, int data, bool & found);
+void print();
 
 int main(){
   
-  int storage[100]; // 100 numbers storage
+  node * root;
+  root = NULL;
   int count = 0; // Amount of actual numbers in storage
   char action;
 
   while (true) {
     
-    cout << "a: Add, b: Peak, c: Delete, d: Print" << endl;
+    cout << "Type A to Add, B to Peak, and C to Print" << endl;
     cin >> action;
 
     switch (action) {
 
-      //Add
-      case 'a':
+      //ADD
+      case 'A':
 
+        cout << "Enter #: ";
         int input;
         cin >> input;
+
         if (input > 999 || input == 0) {
-          cout << "Enter a value between 1-999!" << endl;
+          cout << "INVALID: Enter a # between 1-999!" << endl << endl;
           break;
         }
         if (count > 100) {
-          cout << "Max 100 #s in this heap!" << endl;            break;
+          cout << "100 #s Max" << endl << endl;            break;
         }
-        
+
+        cout << endl;
         count++;
-        storage[count-1] = input;
+        add(root, input);
         break;
 
-      //Print
-      case 'b':
-        peak(storage, count);
+      //PEAK
+      case 'B':
+        cout << "Enter # if it's in the tree: ";
+        int input2;
+        cin >> input2;
+        peak(root, input2, booled);
+        
         break;
       
-      //Print
-      case 'c':
-        deleted(storage, count);
-        break;
-      
-      //Print
-      case 'd':
-        display(storage, count, 1, 1);
+      //PRINT
+      case 'C':
+        
         break;
       
     }
@@ -64,69 +72,43 @@ int main(){
   
 }
 
-// Print out the arrays that is already sorted
-void display(int * input, int size, int depth, int control) {
-  
-  for (int i = 0; i < size; i++) {
-
-    if (input[i] != 0) {
-      cout << input[i] << " ";
-    }
-    
-    // If the current slot is equal to depth we bring       it down a line
-    if (i+1 == depth) {
-      // First, Double Control aka each branch has 2          spots for babies
-      control = control * 2;
-      // Second, add it to depth, which is collecting         the # of slots from previous depths
-      depth = depth+control;
-      cout << endl;
-    }
+//ADD
+node * add(node* & root, int & data) {
+  //when it's the first time, aka when root node is empty you create a new node and add data it it, & create left & right nodes
+  if(root == NULL) {
+    root = new node;
+    root->data = data;
+    root->right = NULL;
+    root->left = NULL;
   }
-
-  cout << endl;
-  
+  else if(root->data < data) {
+    root->right = add(root->right,data);
+  }
+  else {
+    root->left = add(root->left,data);
+  }
+  return root; 
 }
 
-void peak(int * input, int size) {
-  
-  cout << "# you would like to find" << endl;
-  int peak;
-  cin >> peak;
-  for (int i = 0; i < size; i++) {
-    if (input[i] == peak) {
-      cout << peak << " found" << endl;
+//PEAK
+void peak(node *root, int data, bool & found)
+{
+    if (root == NULL || found == true)
+        return;
+    peak(root->right, data, found);
+    if(root->data == data, data, found) {
+      found = true;
     }
-  }
+    peak(root->left, data, found);
+     if(root->data == data) {
+      found = true;
+    }
 
-  cout << endl;
-  
 }
 
-void deleted(int * input, int size) {
-  
-  cout << "# you would like to delete" << endl;
-  int deleted;
-  cin >> deleted;
-  for (int i = 0; i < size; i++) {
-    if (input[i] == deleted) {
-      
-      int temp = i+1;
-      int temp2 = i;
-      while (size > temp) {
-        input[temp2] = input[temp];
-        input[temp] = 0;
-        temp2++;
-        temp++;
-      }
-      cout << deleted << " deleted" << endl;
-    }
-  }
-
-  cout << endl;
-  
-}
+//PRINT
 
 /*
 Sources:
-None
+
 */
